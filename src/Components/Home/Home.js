bc0612cb937eaf6f38e12389d2a4c6834769630b
Isah Jacob <isahjacob0@gmail.com>
2023-10-20 21:13:48 +0100

    modified the home.js file and css file

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -23,7 +23,7 @@ function Home() {
                         <strong className="about-me">FullStack Engineer</strong>            
                     </div>
                     <p className="about-paragraph">
-                    I am an innovative software engineer committed to pushing the limits of technology to provide brilliant results for businesses of all sizes. Beyond my work, I'm similarly committed to helping other professionals and tech enthusiasts along their paths by sharing my expertise through technical writing, lesson development, and interesting YouTube material.
+                        I am an innovative software engineer committed to pushing the limits of technology to provide brilliant results for businesses of all sizes. Beyond my work, I'm similarly committed to helping other professionals and tech enthusiasts along their paths by sharing my expertise through technical writing, lesson development, and interesting YouTube material.
                     </p>
                     <div className="social-button">
                         <a href="https://www.linkedin.com/in/isahjacob">
